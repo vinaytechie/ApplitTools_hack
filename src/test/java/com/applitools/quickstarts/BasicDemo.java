096a8f354d8c5893fed5eaf537ccc856f550764a
Sophie De Paula <sophie24587@gamil.com>
2020-05-02 15:16:02 -0700

    update tutorial according to best practices

--- src/test/java/com/applitools/quickstarts/BasicDemo.java
+++ src/test/java/com/applitools/quickstarts/BasicDemo.java
@@ -1,68 +1,69 @@
 package com.applitools.quickstarts;
 
-import static com.google.common.base.Strings.isNullOrEmpty;
-
 import com.applitools.eyes.*;
+import com.applitools.eyes.selenium.Configuration;
 import com.applitools.eyes.selenium.ClassicRunner;
 import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.fluent.Target;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Runs Applitools test for the demo app https://demo.applitools.com
- */
-@RunWith(JUnit4.class)
+
 public class BasicDemo {
 
-	private EyesRunner runner;
-	private Eyes eyes;
-	private static BatchInfo batch;
-	private WebDriver driver;
+	public static WebDriver driver = null;
+	public static EyesRunner runner = null;
+	public static Eyes eyes = null;
+	public static Configuration config = null;
+	
+	public static void main(String[] args) {
+		
+		setUp();
+		
+		Test();
+		
+		driver.quit();
 
-	@BeforeClass
-	public static void setBatch() {
-		// Must be before ALL tests (at Class-level)
-		batch = new BatchInfo("Demo batch");
+		// Wait and collect all test results
+		TestResultsSummary allTestResults = runner.getAllTestResults();
+
+		// Print results
+		System.out.println(allTestResults);
+		
 	}
-
-	@Before
-	public void beforeEach() {
-		// Initialize the Runner for your test.
-		runner = new ClassicRunner();
-
-		// Initialize the eyes SDK
-		eyes = new Eyes(runner);
-
-		// Raise an error if no API Key has been found.
-		if(isNullOrEmpty(System.getenv("APPLITOOLS_API_KEY"))) {
-		    throw new RuntimeException("No API Key found; Please set environment variable 'APPLITOOLS_API_KEY'.");
-		}
-
-		// Set your personal Applitols API Key from your environment variables.
-		eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
-
-		// set batch name
-		eyes.setBatch(batch);
-
+	
+	public static void setUp() {
 		// Use Chrome browser
 		driver = new ChromeDriver();
-
+	
+		// Initialize the Runner for your test.
+		runner = new ClassicRunner();
+		
+		//Initialize the eyes configuration.
+		config  = new Configuration();
+		
+		// set Api Key from environment variables.
+		config.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
+		
+		// set new batch 
+		config.setBatch(new BatchInfo("Demo batch"));
+		
+		// Initialize the eyes SDK
+		eyes = new Eyes(runner);
+		
+		//set the configuration to eyes
+		eyes.setConfiguration(config);
 	}
 
-	@Test
-	public void basicTest() {
+
+	public static void Test() {
+		try {
 		// Set AUT's name, test name and viewport size (width X height)
 		// We have set it to 800 x 600 to accommodate various screens. Feel free to
 		// change it.
-		eyes.open(driver, "Demo App", "Smoke Test", new RectangleSize(800, 800));
+		eyes.open(driver, "Demo App", "Smoke Test", new RectangleSize(800, 600));
 
 		// Navigate the browser to the "ACME" demo app.
 		driver.get("https://demo.applitools.com");
@@ -70,32 +71,29 @@ public class BasicDemo {
 		// To see visual bugs after the first run, use the commented line below instead.
 		//driver.get("https://demo.applitools.com/index_v2.html");
 				
-		// Visual checkpoint #1 - Check the login page.
-		eyes.checkWindow("Login Window");
+		// Visual checkpoint #1 - Check the login page. using the fluent API 
+		//https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
+		eyes.check("Login Window", Target.window().fully());
 
 		// This will create a test with two test steps.
 		driver.findElement(By.id("log-in")).click();
 
 		// Visual checkpoint #2 - Check the app page.
-		eyes.checkWindow("App Window");
+		eyes.check("App Window", Target.window().fully());
 
 		// End the test.
 		eyes.closeAsync();
+		}
+		catch(Exception e) {
+			driver.quit();
+		}
+		finally {
+			
+			// If the test was aborted before eyes.close was called, ends the test as
+			// aborted.
+			eyes.abortAsync();
+			
+		}
 	}
 
-	@After
-	public void afterEach() {
-		// Close the browser.
-		driver.quit();
-
-		// If the test was aborted before eyes.close was called, ends the test as
-		// aborted.
-		eyes.abortIfNotClosed();
-
-		// Wait and collect all test results
-		TestResultsSummary allTestResults = runner.getAllTestResults();
-
-		// Print results
-		System.out.println(allTestResults);
-	}
 }
