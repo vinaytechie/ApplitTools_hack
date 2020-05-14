706a18a85e9a7ea2377d5ead069c7050edeac61c
Yurii <yurii.cherepovskiy@applitools.com>
2020-05-14 23:15:53 +0300

    Add Junit

--- src/test/java/com/applitools/quickstarts/BasicDemo.java
+++ src/test/java/com/applitools/quickstarts/BasicDemo.java
@@ -9,10 +9,12 @@ import com.applitools.eyes.selenium.fluent.Target;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
 
 public class BasicDemo {
 
-	public static void main(String[] args) {
+	@Test
+        public void test() {
 
 		// Use Chrome browser
 		WebDriver driver = new ChromeDriver();
@@ -58,36 +60,30 @@ public class BasicDemo {
 	}
 
 	private static void TestDemoApp(WebDriver driver, Eyes eyes) {
-		try {
-			// Set AUT's name, test name and viewport size (width X height)
-			// We have set it to 800 x 600 to accommodate various screens. Feel free to
-			// change it.
-			eyes.open(driver, "Demo App", "Smoke Test", new RectangleSize(800, 600));
+		// Set AUT's name, test name and viewport size (width X height)
+		// We have set it to 800 x 600 to accommodate various screens. Feel free to
+		// change it.
+		eyes.open(driver, "Demo App", "Smoke Test", new RectangleSize(800, 600));
 
-			// Navigate the browser to the "ACME" demo app.
-			driver.get("https://demo.applitools.com");
+		// Navigate the browser to the "ACME" demo app.
+		driver.get("https://demo.applitools.com");
 
-			// To see visual bugs after the first run, use the commented line below instead.
-			// driver.get("https://demo.applitools.com/index_v2.html");
+		// To see visual bugs after the first run, use the commented line below instead.
+		// driver.get("https://demo.applitools.com/index_v2.html");
 
-			// Visual checkpoint #1 - Check the login page. using the fluent API
-			// https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
-			eyes.check(Target.window().fully().withName("Login Window"));
+		// Visual checkpoint #1 - Check the login page. using the fluent API
+		// https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html?Highlight=fluent%20api
+		eyes.check(Target.window().fully().withName("Login Window"));
 
-			// This will create a test with two test steps.
-			driver.findElement(By.id("log-in")).click();
+		// This will create a test with two test steps.
+		driver.findElement(By.id("log-in")).click();
 
-			// Visual checkpoint #2 - Check the app page.
-			eyes.check(Target.window().fully().withName("App Window"));
+		// Visual checkpoint #2 - Check the app page.
+		eyes.check(Target.window().fully().withName("App Window"));
 
-			// End the test.
-			eyes.closeAsync();
+		// End the test.
+		eyes.closeAsync();
 
-		} catch (Exception e) {
-			// If the test was aborted before eyes.close was called, ends the test as
-			// aborted.
-			eyes.abortAsync();
-		}
 	}
 	
 	private static void tearDown(WebDriver driver, EyesRunner runner) {
